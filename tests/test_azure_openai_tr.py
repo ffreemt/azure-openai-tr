e2951216471d6571c1f915fce3a8389299b1d531
ffreemt <yucongo+fmt@gmail.com>
2023-07-17 20:02:12 +0800

    Update 0.1.0a0

---
+++ tests/test_azure_openai_tr.py
@@ -0,0 +1,25 @@
+"""Test azure_openai_tr."""
+# pylint: disable=broad-except
+from azure_openai_tr import __version__
+from azure_openai_tr import azure_openai_tr
+
+
+def test_version():
+    """Test version."""
+    assert __version__[:3] == "0.1"
+
+
+def test_sanity():
+    """Check sanity."""
+    try:
+        assert not azure_openai_tr()
+    except Exception:
+        assert True
+
+def test_tr_chinese():
+    """Test translation to Chinese."""
+    assert (
+        "测试" in azure_openai_tr("test") or
+        "试验" in azure_openai_tr("test") or
+        "试验" in azure_openai_tr("test")
+    )
